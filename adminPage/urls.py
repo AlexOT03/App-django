ec4551c5b1241f8e97794182a078dc1b7f8cf52f
Jesus <alexot855@gmail.com>
2023-05-19 14:23:07 -0600

    proyecto estadia, CRUD y Home pages ahora con estilo v0.1.2

--- adminPage/urls.py
+++ adminPage/urls.py
@@ -3,5 +3,8 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('admin/', views.MyView.as_view(), name="my-view"),
+    path('admin/', views.admin.as_view(), name="admin"),
+    path('admin/places', views.adminPlaces.as_view(), name="places"),
+    path('admin/orders', views.adminOrders.as_view(), name="orders"),
+    path('admin/messages', views.adminMessages.as_view(), name="messages"),
 ]
--- adminPage/views.py
+++ adminPage/views.py
@@ -3,6 +3,21 @@ from django.http import HttpResponse, JsonResponse
 from django.views import View
 
 # Create your views here.
-class MyView(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello, World!, Iam Admin!!!!!!!!!!")
+class admin(View):
+    def get(self, request):
+        return render(request, 'indexAdmin.html')
+
+class adminPlaces(View):
+    def get(self, request):
+        return render(request, 'placesAdmin.html')
+    
+    def post(self, request, id):
+        return redirect('places')
+
+class adminOrders(View):
+    def get(self, request):
+        return render(request, 'ordersAdmin.html')
+    
+class adminMessages(View):
+    def get(self, request):
+        return render(request, 'messagesAdmin.html')
